2336f98dffe5d22bc852541529025b2fe51d1629
Michal Olichwer <72801083+olimek@users.noreply.github.com>
2025-01-10 18:48:08 +0100

    working collection view

--- AstroWeather/Pages/MainPage.xaml.cs
+++ AstroWeather/Pages/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using AstroWeather.Helpers;
+﻿//using AndroidX.ConstraintLayout.Helper.Widget;
+using AstroWeather.Helpers;
+using Microsoft.Maui.Controls;
 namespace AstroWeather
 {
 
@@ -8,16 +10,17 @@ namespace AstroWeather
         {
             InitializeComponent();
             WeatherRouter getWeatherinfo = new();
-            var lista = getWeatherinfo.getWeatherinfo();
+            var Pogoda = getWeatherinfo.getWeatherinfo();
             DateTime currentDateTime = DateTime.Now;
-            var result2 = lista.SelectMany(i => i).Distinct();
-
-            ActualTemp.Text = lista[0][Convert.ToInt32(currentDateTime.Hour)].temp.ToString() + " °C";
-            LocalisationListView.ItemsSource = lista[0].Select(kvp => new
+            var result2 = Pogoda.SelectMany(i => i).Distinct();
+            var filteredWeather = result2.Skip(Convert.ToInt32(currentDateTime.Hour)).Take(12).ToList();
+            BindingContext = new { pogoda = filteredWeather };
+            //ActualTemp.Text = lista[0][Convert.ToInt32(currentDateTime.Hour)].temp.ToString() + " °C";
+            /*LocalisationListView.ItemsSource = lista[0].Select(kvp => new
             {
                 Key = kvp.datetime,
                 Value = kvp.cloudcover.ToString()
-            }).ToList();
+            }).ToList();*/
         }
     }
 
