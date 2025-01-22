2149c3361bb69e598237a58283311c36ee35d3a8
Michal Olichwer <72801083+olimek@users.noreply.github.com>
2025-01-22 20:02:59 +0100

    working choose start page

--- AstroWeather/AppShell.xaml.cs
+++ AstroWeather/AppShell.xaml.cs
@@ -1,10 +1,29 @@
-﻿namespace AstroWeather
+﻿using AstroWeather.Pages;
+namespace AstroWeather
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
+            
             InitializeComponent();
+            if (false)
+            {
+                Dispatcher.Dispatch(async () =>
+                {
+                    await Shell.Current.GoToAsync("//MainPage");
+                });
+
+            }
+            else
+            {
+                Dispatcher.Dispatch(async () =>
+                {
+                    await Shell.Current.GoToAsync("//SettingsPage");
+                });
+
+            }
+            
         }
     }
 }
